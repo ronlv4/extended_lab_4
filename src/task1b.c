2b1cd15b77fceae6b1a7add4fd76c1a74aa96ba0
Ron Levi <t-ronlevi@microsoft.com>
2022-05-10 13:52:55 +0300

    supposed to work

--- src/task1b.c
+++ src/task1b.c
@@ -71,7 +71,7 @@ int main(int argc, char **argv) {
         if (!strncmp(argv[arg_index], "-i", 2)) {
             input_fd = system_call(SYS_OPEN, argv[arg_index] + 2, O_RDONLY, 0777);
             print_if_debug_mode_char(debug_mode, SYS_OPEN, argv[arg_index] + 2, input_fd);
-            if (input_fd == -1)
+            if (input_fd < 0)
             {
                 system_call(SYS_WRITE, STDERR, "'open' system call returned an error\nexiting...", 48);
                 system_call(SYS_EXIT, 1);
