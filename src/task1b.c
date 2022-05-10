5c3066b6d4256771c4d83ce66ba87b5d23156ebd
Ron Levi <t-ronlevi@microsoft.com>
2022-05-10 10:05:24 +0300

    some var naming changes

--- src/task1b.c
+++ src/task1b.c
@@ -8,7 +8,8 @@
 #define SYS_EXIT 1
 #define SYS_READ 3
 #define SYS_WRITE 4
-#define NEW_LINE '\n'
+
+system_call();
 
 void check_free_mem()
 {
@@ -18,13 +19,6 @@ void check_free_mem()
 	system_call(SYS_WRITE, STDERR, itoa(2), 1);
 }
 
-void print_if_debug_mode(int debug_mode, int call_id, int arg1, int return_code, int out_name)
-{
-	if (debug_mode)
-	{
-		print_system_call(call_id, arg1, return_code, out_name);
-	}
-}
 
 void print_system_call(int call_id, int arg1, int return_code, int out_name)
 {
@@ -52,17 +46,24 @@ void print_system_call(int call_id, int arg1, int return_code, int out_name)
 	}
 }
 
+void print_if_debug_mode(int debug_mode, int call_id, int arg1, int return_code, int out_name)
+{
+	if (debug_mode)
+	{
+		print_system_call(call_id, arg1, return_code, out_name);
+	}
+}
 int main(int argc, char **argv)
 {
 	int arg_index = 1;
 	int debug_mode = 0;
-	int outfile = 0;
-	int infile = 0;
-	char *file_in;
-	char *out_file;
+	int output_fd = STDOUT;
+	int input_fd = STDIN;
+	char *input_file;
+	char *output_file;
 	int fp;
 	int fout = 0;
-	file_in = argv[arg_index] + 2;
+    input_file = argv[arg_index] + 2;
 	while (arg_index < argc)
 	{
 		if (!strcmp(argv[arg_index], "-D"))
@@ -71,20 +72,20 @@ int main(int argc, char **argv)
 		}
 		else if (!strncmp(argv[arg_index], "-i", 2))
 		{
-			infile = 1;
-			file_in = argv[arg_index] + 2;
-			fp = system_call(5, file_in, 0, 0777);
+            input_fd = 1;
+            input_file = argv[arg_index] + 2;
+			fp = system_call(5, input_file, 0, 0777);
 		}
 		else if (!strncmp(argv[arg_index], "-o", 2))
 		{
-			outfile = 1;
-			out_file = argv[arg_index] + 2;
-			fout = system_call(5, out_file, 1 | 64, 0777);
+            output_fd = 1;
+            output_file = argv[arg_index] + 2;
+			fout = system_call(5, output_file, 1 | 64, 0777);
 			print_if_debug_mode(debug_mode, 5, STDOUT, 1, fout);
 		}
 		arg_index++;
 	}
-	if (infile)
+	if (input_fd)
 	{
 		print_if_debug_mode(debug_mode, 5, STDOUT, 1, fout);
 	}
@@ -97,7 +98,7 @@ int main(int argc, char **argv)
 	int return_code;
 	int out = 0;
 	int in = 0;
-	if (infile = 1)
+	if (input_fd == 1)
 	{
 		bytes_read = system_call(SYS_READ, fp, buf, 1);
 		print_if_debug_mode(debug_mode, SYS_READ, STDIN, bytes_read, fout);
@@ -111,7 +112,7 @@ int main(int argc, char **argv)
 	{
 		while (buf[0] == ' ' && strcmp(buf, new_line))
 		{
-			if (infile = 1)
+			if (input_fd = 1)
 			{
 				bytes_read = system_call(SYS_READ, fp, buf, 1);
 				print_if_debug_mode(debug_mode, SYS_READ, STDIN, bytes_read, fout);
@@ -129,7 +130,7 @@ int main(int argc, char **argv)
 		}
 		while (buf[0] != ' ' && bytes_read != 0 && strcmp(buf, new_line))
 		{
-			if (infile = 1)
+			if (input_fd = 1)
 			{
 				bytes_read = system_call(SYS_READ, fp, buf, 1);
 				print_if_debug_mode(debug_mode, SYS_READ, STDIN, bytes_read, fout);
